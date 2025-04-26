49293b0999ee11ea43480a1e06cd9d0533625006
azeke <azekeprofit@gmail.com>
2025-04-26 17:44:34 +0500

    rotation

--- src/lib/Cube.svelte
+++ src/lib/Cube.svelte
@@ -1,23 +1,26 @@
 <script lang="ts">
   type vector3 = [number, number, number];
   import { T } from "@threlte/core";
-  import { Edges, Outlines } from "@threlte/extras";
-  const size=1;
-  import {DoubleSide,Vector3,Euler, MeshBasicMaterial} from 'three';
+  import { Edges } from "@threlte/extras";
+  import { MeshBasicMaterial } from "three";
 
-  const { position,rotation,color,colorSideA,colorSideB}:{position:vector3,rotation:vector3,color?:string,colorSideA?:string,colorSideB?:string}=$props();
-  let m1:any=new MeshBasicMaterial({color}); // this shuts up Typescript
-  let m2=new MeshBasicMaterial({color:colorSideA});
-  let m3=new MeshBasicMaterial({color:colorSideB});
+  const {
+    position,
+    color,
+    colorSideA,
+    colorSideB,
+  }: {
+    position: vector3;
+    color?: string;
+    colorSideA?: string;
+    colorSideB?: string;
+  } = $props();
+  let m1: any = new MeshBasicMaterial({ color }); // this shuts up Typescript
+  let m2 = new MeshBasicMaterial({ color: colorSideA });
+  let m3 = new MeshBasicMaterial({ color: colorSideB });
 </script>
 
-
-<T.Mesh {position} {rotation} material={[m2,m2,m1,m1,m3,m3]}>
-  <T.BoxGeometry args={[size, size,size]} />
-  
-  <Edges
-  color="black"
-  thickness={1}
-/>
+<T.Mesh {position} material={[m2, m2, m1, m1, m3, m3]}>
+  <T.BoxGeometry args={[1, 1, 1]} />
+  <Edges color="black" thickness={1} />
 </T.Mesh>
-
--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -1,9 +1,8 @@
-<script>
+<script lang="ts">
   import { T, useTask } from "@threlte/core";
   import { OrbitControls } from "@threlte/extras";
-  // import { Checkbox, Pane, ThemeUtils, Slider } from "svelte-tweakpane-ui";
+// import { Checkbox, Pane, ThemeUtils, Slider } from "svelte-tweakpane-ui";
   import Cube from "./Cube.svelte";
-  import { Group } from "three";
 
   const COLORS = {
     white: "#FFFFFF",
@@ -14,11 +13,14 @@
     green: "#00FF00",
   };
 
-  const positions = [-1, 0, 1];
+  const positions = [-1, 0, 1] as const;
   let x = -6.087414455248885;
   let y = 2.4046333628934424;
   let z = 2.335561018356455;
 
+type rotAxisType='x'|'y'|'z';
+let rotAxis=$state<rotAxisType>('z');
+const whichAxis:typeof positions[number]=$state(1);
   let rotation = $state(0);
   useTask((delta) => {
     rotation += delta
@@ -48,20 +50,62 @@
     bind:value={z}
   />
 </Pane> -->
+{#if rotAxis=='x'}
 {#each positions as x}
-<T.Group rotation.x={x==0?rotation:0}>
+<T.Group rotation.x={x==whichAxis?rotation:0}>
   {#each positions as y}
     {#each positions as z}
       {#if !(x == 0 && x == y && y == z)}
         <Cube
           position={[x, y, z]}
-          rotation={[0, 0, 0]}
-          color={{ [-1]: COLORS.yellow, 1: COLORS.white }[y]}
-          colorSideA={{ [-1]: COLORS.blue, 1: COLORS.green }[x]}
-          colorSideB={{ [-1]: COLORS.orange, 1: COLORS.red }[z]}
+          color={{ [-1]: COLORS.yellow, 0:'maroon', 1: COLORS.white }[y]}
+          colorSideA={{ [-1]: COLORS.blue, 0:'maroon', 1: COLORS.green }[x]}
+          colorSideB={{ [-1]: COLORS.orange, 0:'maroon', 1: COLORS.red }[z]}
         />
       {/if}
     {/each}
   {/each}
 </T.Group>
 {/each}
+{/if}
+
+{#if rotAxis=='y'}
+{#each positions as y}
+<T.Group rotation.y={y==whichAxis?rotation:0}>
+  {#each positions as x}
+    {#each positions as z}
+      {#if !(x == 0 && x == y && y == z)}
+        <Cube
+          position={[x, y, z]}
+          color={{ [-1]: COLORS.yellow, 0:'maroon', 1: COLORS.white }[y]}
+          colorSideA={{ [-1]: COLORS.blue, 0:'maroon', 1: COLORS.green }[x]}
+          colorSideB={{ [-1]: COLORS.orange, 0:'maroon', 1: COLORS.red }[z]}
+        />
+      {/if}
+    {/each}
+  {/each}
+</T.Group>
+{/each}
+{/if}
+
+
+
+{#if rotAxis=='z'}
+{#each positions as z}
+<T.Group rotation.z={z==whichAxis?rotation:0}>
+  {#each positions as x}
+    {#each positions as y}
+      {#if !(x == 0 && x == y && y == z)}
+        <Cube
+          position={[x, y, z]}
+          color={{ [-1]: COLORS.yellow, 0:'maroon', 1: COLORS.white }[y]}
+          colorSideA={{ [-1]: COLORS.blue, 0:'maroon', 1: COLORS.green }[x]}
+          colorSideB={{ [-1]: COLORS.orange, 0:'maroon', 1: COLORS.red }[z]}
+        />
+      {/if}
+    {/each}
+  {/each}
+</T.Group>
+{/each}
+{/if}
+
